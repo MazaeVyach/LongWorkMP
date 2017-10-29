a35dc6f14654e44a8314e456e1da02bfc0eea0c9
GinoCavalli <ginocavalli359@gmail.com>
2017-10-30 00:20:32 +0300

    Создал ветвь для агента

--- LongWorkMP/Agent.Interface/Program.cs
+++ LongWorkMP/Agent.Interface/Program.cs
@@ -1,9 +1,11 @@
 ﻿namespace Agent.Interface
 {
+    using System;
     public class Program
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine("Hello!");
         }
     }
 }
