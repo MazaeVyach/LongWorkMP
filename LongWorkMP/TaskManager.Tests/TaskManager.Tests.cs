0b6e7a180c38fc7942ce5961ef20766f38f33c52
GinoCavalli <ginocavalli359@gmail.com>
2017-11-09 10:01:48 +0300

    Добавление функционала Task Manager

--- LongWorkMP/TaskManager.Tests/TaskManager.Tests.cs
+++ LongWorkMP/TaskManager.Tests/TaskManager.Tests.cs
@@ -1,7 +1,29 @@
 ﻿
 namespace TaskManager.Tests
 {
-    public class Class1
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class Tests
     {
+        /*[Test]
+        public void SearchTests0()
+        {
+            string str1 = "602175614209757-12489";
+            string str2 = "5-7237561420------43608357";
+            string resultStr = SearchCommonSubString.Search(str1, str2);
+            string expectedStr = "7561420";
+            Assert.That(expectedStr, Is.EqualTo(resultStr));
+        }
+
+        [Test]
+        public void SearchTests1()
+        {
+            string str1 = "Моя мама мыла раму";
+            string str2 = "Твоя мама мыла малыша";
+            //string resultStr = SearchCommonSubString.Search(str1, str2);
+            string expectedStr = "оя мама мыла ";
+            //Assert.That(expectedStr, Is.EqualTo(resultStr));
+        }*/
     }
 }
--- LongWorkMP/TaskManager/TaskManager.cs
+++ LongWorkMP/TaskManager/TaskManager.cs
@@ -1,7 +1,64 @@
 ﻿
 namespace TaskManager
 {
-    public class TaskManager
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Task for calculate.
+    /// </summary>
+    public class Task
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Task"/> class.
+        /// </summary>
+        /// <param name="beginValue">
+        /// The begin value.
+        /// </param>
+        /// <param name="endValue">
+        /// The end value.
+        /// </param>
+        public Task(long beginValue, long endValue)
+        {
+            this.BeginValue = beginValue;
+            this.EndValue = endValue;
+        }
+
+        /// <summary>
+        /// Gets or sets the begin value.
+        /// </summary>
+        private long BeginValue { get; set; }
+
+        /// <summary>
+        /// Gets or sets the end value.
+        /// </summary>
+        private long EndValue { get; set; }
+    }
+
+    public class Class1
+    {
+        private SortedDictionary<int, Task> GetTaskDictionary(string beginValueStr, string endValueStr, int taskSize)
+        {
+            long beginValue = 0; // = Alphabet.StringToNumber(beginValueStr)
+            long endValue = 0; // = Alphabet.StringToNumber(endValueStr)
+
+            SortedDictionary<int, Task> taskDictionary = new SortedDictionary<int, Task>();
+
+            long beginRange = beginValue;
+            long endRange = beginValue + taskSize;
+
+            while (beginRange <= endValue)
+            {
+                if (beginRange + taskSize > endValue)
+                    endRange = endValue;
+                else
+                    endRange += taskSize;
+
+                Task task = new Task(beginRange, endRange);
+
+                beginValue = endRange + 1;
+            }
+
+            return taskDictionary;
+        }
     }
 }
